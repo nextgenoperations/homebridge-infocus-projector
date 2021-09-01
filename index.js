208a67ac828f649467f25aad6b00379ddb2f1dcc
ecoen66 <ecoen@yahoo.com>
2021-09-01 10:25:18 -0500

    Test for Axios
    Testing with Axios

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const request = require("request");
+const axios = require('axios');
 
 const command_path = "/cgi-bin/directsend?";
 
@@ -14,7 +14,7 @@ var Characteristic;
 module.exports = function (homebridge) {
     Service = homebridge.hap.Service;
     Characteristic = homebridge.hap.Characteristic;
-    homebridge.registerAccessory("homebridge-epson-projector", "EpsonProjector", EpsonProjector);
+    homebridge.registerAccessory("homebridge-epson-projector", "Epson Projector", EpsonProjector);
 };
 
 
@@ -24,20 +24,27 @@ function EpsonProjector(log, config) {
     this.model = config["model"] === undefined ? "" : config["model"];
     this.serial = config["serial"] === undefined ? "" : config["serial"];
     this.name = config["name"];
+    this.timeout = config["timeout"] === undefined ? timeout : config["timeout"];
+    this.debug = config["debug"] === undefined ? debug : config["debug"];
+		this.api = axios.create({
+			baseURL: "http://" + this.ip,
+			timeout: this.timeout,
+			headers: {'Referer'': "http://" + this.ip + "/cgi-bin/webconf"}
+		});
+
 }
 
 EpsonProjector.prototype = {
 
     getPowerState: function (callback) {
-        if (debug) {
+        if (this.debug) {
             console.log(error);
         }
-        request.get({
-            uri: "http://" + this.ip + query_path + "PWR?",
-            headers: {
-                "Referer": "http://" + this.ip + "/cgi-bin/webconf",
-            },
-            timeout: timeout
+        
+
+
+        this.api.get({
+            url: query_path + "PWR?"
         }, function (error, response, body) {
             if (error !== null) {
                 callback(error);
@@ -58,17 +65,13 @@ EpsonProjector.prototype = {
         } else {
             command = "PWR=OFF";
         }
-        if (debug) {
+        if (this.debug) {
             console.log(error);
         }
-        request.get({
-            uri: "http://" + this.ip + command_path + command,
-            headers: {
-                "Referer": "http://" + this.ip + "/cgi-bin/webconf",
-            },
-            timeout: timeout
+        this.api.get({
+            url: command_path + command
         }, function (error, response, body) {
-            if (debug) {
+            if (this.debug) {
                 console.log(error);
             }
             callback();
